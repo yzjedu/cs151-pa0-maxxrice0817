f7463246fc6fa2aa0eeea1a3fa1a7d261501c0c7
maxrice <mxrice@loyola.edu>
2024-09-17 18:36:51 -0400

    my PA00 code

---
+++ PA00.py
@@ -0,0 +1,11 @@
+#Programer: Max Rice Problem: Calculate grade needed on final weighted 20% to get 90% in whole course
+Current_Grade = float(input("Current Grade: "))
+#My sisters goal was to get a 90% in the class
+Desired_Grade = 90
+#Test was weighted 20% so I made it 0.2 so it would work with the calculation
+Final_Exam_Weight = 0.20
+
+# will print the grade needed to get a 90% in the class along with a clarifying message beforehand
+print("You will need at least a:",((Desired_Grade - ((1 - Final_Exam_Weight) * Current_Grade)) / Final_Exam_Weight))
+
+
